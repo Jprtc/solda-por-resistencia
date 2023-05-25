a49fae9497d92bccf4ad1ba36427201cd2f41030
Jprtc <jprtc8@gmail.com>
2023-05-25 02:36:27 -0300

    Fixed problem with wrongly named components instead of Components on tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/widgets/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  enabled: process.env.NODE_ENV === "production",
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
 };
